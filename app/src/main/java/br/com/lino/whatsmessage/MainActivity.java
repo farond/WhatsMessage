aa42f408ede852cb6a7d4b7bf79866440b5e27a8
farond <fabio_lino@outlook.com>
2023-02-26 21:39:31 -0300

    Implementando modificações no código, para código de DDD dos estados brasileiros em um spinner. Mudanças no layout.

--- app/src/main/java/br/com/lino/whatsmessage/MainActivity.java
+++ app/src/main/java/br/com/lino/whatsmessage/MainActivity.java
@@ -7,9 +7,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.hbb20.CountryCodePicker;
@@ -21,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextTelefone;
     EditText editTextMensagem;
     public Button enviarButton;
-    String messagestr, phonestr = "";
+    String messagestr, phonestr = "", dddSelecionado;
+    String ddd;
+
+    Spinner ddiSpiner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +39,36 @@ public class MainActivity extends AppCompatActivity {
         editTextMensagem = findViewById(R.id.mensagemEditText);
         enviarButton = findViewById(R.id.enviarBtn);
 
+        ddiSpiner = findViewById(R.id.ddi_spinner);
+        ddiSpiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("ITEMGENEROSELECIONADO", "POSIÇÃO-->" + position);
+                ddiSpiner.setSelection(((ArrayAdapter) ddiSpiner.getAdapter()).getPosition(ddd));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         enviarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 messagestr = editTextMensagem.getText().toString();
                 phonestr = editTextTelefone.getText().toString();
+                dddSelecionado = ddiSpiner.getSelectedItem().toString();
 
                 if (!messagestr.isEmpty() && !phonestr.isEmpty()) {
 
                     if (appInstalledOrNot("com.whatsapp")) {
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://api.whatsapp.com/send?phone=" + "+55" + phonestr + "&text=" + messagestr));
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://api.whatsapp.com/send?phone=" + "+55" + dddSelecionado + phonestr + "&text=" + messagestr));
                         startActivity(intent);
                         editTextMensagem.setText("");
                         editTextTelefone.setText("");
                     } else {
-                        Toast.makeText(MainActivity.this, "Whatsapp não está intalado neste aparelho", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Whatsapp não está instalado neste aparelho", Toast.LENGTH_SHORT).show();
                     }
 
                 } else {
